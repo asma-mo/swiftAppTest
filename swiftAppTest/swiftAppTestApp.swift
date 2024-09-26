9d87d9a7e9c27ff99914b8a5cc719971137b9339
asma <Asma.mo@outlook.com>
2024-09-26 17:05:33 +0300

    Initial Commit

---
+++ swiftAppTest/swiftAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftAppTestApp.swift
+//  swiftAppTest
+//
+//  Created by Asma on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftAppTestTests/swiftAppTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftAppTestTests.swift
+//  swiftAppTestTests
+//
+//  Created by Asma on 26/09/2024.
+//
+
+import Testing
+@testable import swiftAppTest
+
+struct swiftAppTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
